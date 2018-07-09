b9039dd5fcf51fcf4104d946ee6df3551e4a9cd1
Humad <humad@live.com>
2018-07-09 13:05:13 -0700

    added terminal cursor, styling changes

--- js/freelancer.js
+++ js/freelancer.js
@@ -73,3 +73,12 @@
   });
 
 })(jQuery); // End of use strict
+
+$(function(){
+  $('.product-card').hover(function() {
+     $(this).find('.description').animate({
+       height: "toggle",
+       opacity: "toggle"
+     }, 300);
+   });
+});
